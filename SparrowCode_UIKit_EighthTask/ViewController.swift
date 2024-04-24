df6dbb4987ea98e82a9cf47e03890ca173b4f742
Edmond <edpodlegaev@yandex.ru>
2024-04-24 20:21:52 +0300

    Initial Commit

---
+++ SparrowCode_UIKit_EighthTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SparrowCode_UIKit_EighthTask
+//
+//  Created by Edmond Podlegaev on 24.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
