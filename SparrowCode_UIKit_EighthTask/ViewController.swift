70472f896f37e4bbb1d03b901133417b66b506dd
Edmond <edpodlegaev@yandex.ru>
2024-04-24 20:40:48 +0300

    Complete

--- SparrowCode_UIKit_EighthTask/ViewController.swift
+++ SparrowCode_UIKit_EighthTask/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "person.crop.circle.fill")
+        imageView.tintColor = .lightGray
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+
+        return imageView
+    }()
+
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.frame = view.frame
+        scrollView.contentSize = .init(width: view.frame.width, height: 1000)
+
+        return scrollView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addSubview(scrollView)
+
+        setupNavigationController()
+        setupImageView()
     }
 
+    func setupNavigationController() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Avatar"
+    }
 
+    func setupImageView() {
+        guard let largeTitleClass = NSClassFromString("_UINavigationBarLargeTitleView") else { return }
+        DispatchQueue.main.async { [self] in
+            self.navigationController?.navigationBar.subviews.forEach { subview in
+                guard subview.isKind(of: largeTitleClass.self) else { return }
+                subview.addSubview(self.imageView)
+                NSLayoutConstraint.activate([
+                    self.imageView.heightAnchor.constraint(equalToConstant: 36),
+                    self.imageView.widthAnchor.constraint(equalToConstant: 36),
+                    self.imageView.bottomAnchor.constraint(equalTo: subview.bottomAnchor, constant: -8),
+                    self.imageView.trailingAnchor.constraint(equalTo: subview.trailingAnchor, constant: -16)
+                ])
+            }
+        }
+    }
 }
-
